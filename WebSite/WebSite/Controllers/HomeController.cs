32302239bc219061320c62d09ff5db8cd37e9da9
Григорий <grigoriy_vs@mail.ru>
2021-02-23 16:59:56 +0300

    files area added with errors

--- WebSite/WebSite/Controllers/HomeController.cs
+++ WebSite/WebSite/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WebSite.Models;
@@ -12,10 +14,15 @@ namespace WebSite.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment _oHostingEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public HomeController(IWebHostEnvironment oHostingEnvironment)
         {
-            _logger = logger;
+            _oHostingEnvironment = oHostingEnvironment;
         }
 
         public IActionResult Index()
@@ -27,11 +34,37 @@ namespace WebSite.Controllers
         {
             return View();
         }
+        public IActionResult Others()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult UploadFiles() 
+        {
+            long size = 0;
+            var files = Request.Form.Files;
+
+            foreach (var file in files)
+            {
+                string fileName = _oHostingEnvironment.WebRootPath + $@"\Files\{file.FileName}";
+                size += file.Length;
+
+                using (FileStream fs = System.IO.File.Create(fileName)) 
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+            }  
+
+            string message = $"{files.Count} files and {size} bytes upload.";
+            return Json(message);
         }
     }
 }
